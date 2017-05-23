e6c2dd9ba5d9763acc34c6cbbcafa62add2f1ae9
Stewart Hetherington <stewart.hetherington.2016@mumail.ie>
2017-05-23 18:53:20 +0100

    Update Quote and images

--- Random_Quote_Generator/javascript.js
+++ Random_Quote_Generator/javascript.js
@@ -42,6 +42,33 @@ var quotes = [
   '\"It\'s like inception, so if it\'s confusing and stupid then so is everyone\'s favourite movie.\" <br> - Rick',
   '\"What, so everyone\'s supposed to sleep every single night now. You realize that nighttime makes up half of all time.\" <br> - Rick',
   
+  //pokemon
+  //Mewtwo
+  '\"I see now that the circumstances of one\'s birth ar irrelevant. It is what you do with the gift of life that determines who you are.\"<br> - Mewtwo',
+  '\"Humans may create me, but they cannot enslave me. This cannot be my destiny!\"<br> - Mewtwo',
+  //Ash
+  '\"Cause i always play to win!\"<br> - Ash Ketchum',
+  '\"I\'m always hungry and that\'s that!\"<br> - Ash Ketchum',
+  ' \"If anybody\'s out there, you can come out. And if you\'re a monster or a ghost, you can stay where you are.\"<br> - Ash Ketchum',
+  //Gary
+  '\"Well ash ya snooze la lose and you\'re behind right from the start\"<br> - Gary Oak',
+  '\"Sometimes I wonder why I was cursed with this talent.\"<br> - Gary Oak',
+  //Random trainer
+  '\"If you need to make a difficult decision and you let someone else decide for you, you will regret it, no matter how it turns out\"<br> - some pokemon trainer on Route 104',
+  '\"Living is using the time given to you.<br>You cannot recall lost time.<br>Don\'t forget that.<br> - random pokemon trainer in Driftveil City',
+  '\"If we live a monotonous life,<br>do we get used to it and stop thinking about changing it?\"<br> - random pokemon trainer in Icirrus City',
+  '\"The more wonderful the meeting,<br>the sadder the parting\"<br> - some pokemon trainer on Stark Mountain',
+  '“That BILL! I heard that he\’ll do whatever it takes to get rare Pokemon. He\’s not above doing all sorts of things, I\’ve heard.\"<br> - man in Cerulian City pokemon center',
+  '\“Develop amnesia conveniently and forget everything you heard!\”<br> - Team Rocket grunt at the Sevii Islands',
+  
+  //Elite 4
+  '\"Even if you lose in battle, if you surpass what you\'ve done before, you have bested yourself.\<br> - Marshal, Unove Elite Four"',
+  '\"A warrior does not dash off in pursuit of another victory, nor does one throw a fit when experiencing a loss. A real warrior ponders the next battle.\<br> - Grimsley, Unova Elite Four”',
+  //Professor Oak
+  '\“This is my grandson. He\’s been your rival since you were a baby. …Erm, what is his name again?\”<br> - Professor Oak',
+  
+  
+  
   
 ]
 var randomNum = 0;
@@ -55,13 +82,13 @@ function changeBGImage(){
     document.body.background = "https://www.walldevil.com/wallpapers/a59/3999-wallpaper-adventure-desktop-images.jpg";
     //Jake
   }else if( randomNum < 5) {
-    document.body.background = "https://i.stack.imgur.com/xHWG8.jpg";
+    document.body.background = "http://wallpapercave.com/wp/d422Ff6.png";
      //bubblegum
   }else if( randomNum < 7) {
-    document.body.background = "https://i.ytimg.com/vi/Vh76l5r-rmY/maxresdefault.jpg";
+    document.body.background = "http://images6.fanpop.com/image/photos/35500000/AT-fanart-Classics-forever-adventure-time-35501542-1800-1200.jpg";
      //Jake jr.
   }else if( randomNum < 8) {
-    document.body.background = "http://vignette3.wikia.nocookie.net/adventuretimewithfinnandjake/images/d/d0/S5e24_Jake_Jr._and_Cinnamon_Bun.png/revision/latest?cb=20140215024700";
+    document.body.background = "http://wallpapercave.com/wp/qGqqaGu.png";
      //Finn
   }else if( randomNum < 12) {
     document.body.background = "http://wallpaper-gallery.net/images/adventure-time-wallpaper-hd/adventure-time-wallpaper-hd-23.jpg";
@@ -85,7 +112,27 @@ function changeBGImage(){
     document.body.background = "https://images8.alphacoders.com/642/thumb-1920-642527.jpg";
     //Rick
   } 
-  else{
+  else if (randomNum < 25) {
     document.body.background = "https://images2.alphacoders.com/642/thumb-1920-642540.png";
-  }   
+    
+    //Pokemon
+    //Mewtwo   
+  }else if (randomNum < 27) {
+    document.body.background = "http://wallpapercave.com/wp/J5mqL59.jpg";  
+  //Ash          
+  }else if (randomNum < 30) {
+    document.body.background = "http://wallpapercave.com/wp/flsEpjC.jpg";  
+  //Gary
+  }else if (randomNum < 32) {
+    document.body.background = "https://images7.alphacoders.com/592/thumb-1920-592675.jpg";  
+  //Random trainer 
+  }else if (randomNum < 38) {
+    document.body.background = "https://s-media-cache-ak0.pinimg.com/originals/31/84/f4/3184f459a94ef64fea2ca422bc7299a9.jpg";  
+  //Elite four       
+  }else if (randomNum < 40) {
+    document.body.background = "http://www.cartoondistrict.com/wp-content/uploads/2014/12/pokemon-wallpaper-HD-for-desktop-17.jpg";  
+  //Professor Oak     
+  }else if (randomNum < 41) {
+    document.body.background = "http://wallpapersdsc.net/wp-content/uploads/2016/09/Pikachu-HD-Wallpaper.jpg";  
+  } 
 }
